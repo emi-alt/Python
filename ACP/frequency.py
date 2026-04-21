dc1aca3053b69ebfd000d9d7859d993764f8a941
emi-alt <zareenomera247@gmail.com>
2026-04-21 14:51:55 +0530

    freq

--- ACP/frequency.py
+++ ACP/frequency.py
@@ -1,7 +1,7 @@
-dict = {1:"hello", 2:"tiara", 3:"nimo"}
+dict = {1:"hello", 2:"tiara", 3:"nimo", 4:"nimo", 5:"tiara", 6:"tiara"}
 print(dict)
 freq = 0
-word = input("Please enter the value of which you want to check the frequency of: ")
+word = input("Please enter the word of which you want to check the frequency of: ")
 for f, w in dict.items():
     if w == word:
         freq += 1
