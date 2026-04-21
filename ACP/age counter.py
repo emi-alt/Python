4f873558ebf0a294a6c4d5007f0208d85cf208e8
emi-alt <zareenomera247@gmail.com>
2026-04-21 16:41:50 +0530

    age counter

--- ACP/age counter.py
+++ ACP/age counter.py
@@ -1,2 +1,17 @@
 age = input("Please enter your age: ")
-if 
+try:
+    if int(age) % 2 == 0:
+        print("Your age is an even number, ", age)
+    elif int(age) % 2 != 0:
+        print("Your age is an odd number, ", age)
+    
+    if int(age) < 10:
+        print("You are too young to be using a device.")
+    elif int(age) > 140:
+        print("Enter your real age.")
+except ValueError:
+    print("Please enter an integer/whole number.")
+except ZeroDivisionError:
+    print("Division by zero is not possible.")
+except:
+    print("Something went wrong.")
