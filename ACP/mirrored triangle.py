c01e74a06913a74a37c32acbbd4fd22c2e947c55
emi-alt <zareenomera247@gmail.com>
2026-05-02 13:52:20 +0530

    mirrored triangle

--- ACP/mirrored triangle.py
+++ ACP/mirrored triangle.py
@@ -1,6 +1,10 @@
 n = int(input("Enter the number of rows: "))
 i = 0
-for i in range(n):
-    for j in range(i+1):
+for i in range(1, n+1):
+    for j in range(i):
         print("*", end=" ")
-        print()
+    print()
+for p in range(n-1, 0,-1):
+    for t in range(p):
+        print("*", end=" ")
+    print()
