e8fb7eb8e74b28639eaac30e7166713a383d40b8
emi-alt <zareenomera247@gmail.com>
2026-05-23 20:56:09 +0530

    squareroot

--- ACP/square root.py
+++ ACP/square root.py
@@ -1,6 +1,3 @@
-num = float(input("Enter a number to find it's square root: "))
-n = 1
-while n > 0 :
-    n += 1
-    if num/n 
-    print("The number is not a perfect square.")
+num = int(input("Enter a number to find it's square root: "))
+root = num**0.5
+print(f"The square root of {num} is {root}.")
