ef5af8293433586a37b02fd5d1cb77ff263b7bd0
emi-alt <zareenomera247@gmail.com>
2026-05-23 23:53:48 +0530

    square it out!

--- ACP/square it out.py
+++ ACP/square it out.py
@@ -1,1 +1,24 @@
-num = input 
+n1 = input("Enter the start of the range of numbers: ")
+n2 = input("Enter the end of the range of numbers: ")
+ul = int(n1)
+ll = int(n2)
+nums = " "
+squares = []
+def square(ul, ll):
+    n = []
+    for i in range(ul, ll+1):
+        n.append(i**2)
+    return n
+squares.extend(square(ul, ll))
+print("Square of the range of numbers is ", squares)
+even = []
+odd = []
+for i in squares:
+    if i%2 == 0:
+        print(f"The number {i} is an even number.")
+        even.append(i)
+    else:
+        print(f"The number {i} is an odd number.")
+        odd.append(i)
+print(f"The list of even numbers is {even}.")
+print(f"The list of odd numbers is {odd}.")
