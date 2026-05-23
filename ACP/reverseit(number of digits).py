174a880553da179306840a430bf426415c2373d2
emi-alt <zareenomera247@gmail.com>
2026-05-23 21:12:15 +0530

    reverse it!

---
+++ ACP/reverseit(number of digits).py
@@ -0,0 +1,12 @@
+import 
+number = input("Enter a number to count it's digits: ")
+count = 0
+for i in number:
+    if int(i) > 0:
+        count += 1
+    elif int(i) == 0:
+        print("No digits")
+    else:
+        print("Enter a valid number.")
+
+print(f"The number of digits in the number {number} are {count}.")
